aeedc3543976b27fc35f88fe1660992516915d74
madiha1012 <madikhan2310@gmail.com>
2022-10-16 19:24:54 +0500

    fifthCommit

--- main.dart
+++ main.dart
@@ -1,5 +1,26 @@
 void main() {
-  print("Madiha Khan");
-  print("Madiha Khan 2");
-  print("My new Git");
+  print("List / Array");
+  List mylist = [11, 22, 33, 44, 55, 66, 77, 88, 99, 100];
+  print(mylist);
+
+  print("Is Empty: ${mylist.isEmpty}");
+
+  print("List Length: ${mylist.length}");
+
+  print("Reversed List: ${mylist.reversed}");
+
+  print("List first Element: ${mylist.first}");
+
+  print("List Last Element: ${mylist.last}");
+
+  mylist.add(111);
+  print(mylist);
+
+  print(mylist.remove(33));
+  print(mylist);
+
+  print(mylist.sublist(1, 2));
+
+  print(mylist.length);
+  
 }
